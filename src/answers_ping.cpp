3414ef7d15922da9f1fbbe6b858b72f0a430f2f7
Javier Rollon <J4p032@gmail.com>
2026-02-03 17:46:41 +0100

    ping ver si msg compuesto esta bien

--- src/answers_ping.cpp
+++ src/answers_ping.cpp
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 16:02:52 by jrollon-          #+#    #+#             */
-/*   Updated: 2026/02/03 16:18:24 by jrollon-         ###   ########.fr       */
+/*   Updated: 2026/02/03 17:45:40 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 void IRCServ::answer_ping(IRCMessage & msg, int fd) {
 
 	std::string ans;
-	std::cout << ans << std::endl;
+
+	vectorIteratorPairType params = msg.getParameters();
+	ans = std::string("PONG ") + "irc.42server.local" + " :" + *(params.first) + "\r\n"; 
+	
 	queue_and_send(fd, ans);
 }
