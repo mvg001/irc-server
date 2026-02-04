5f225cfc2d7f58d6bbbe6fccea5ea6e6ee2d930e
Javier Rollon <J4p032@gmail.com>
2026-02-04 11:34:26 +0100

    ping con msg de error

--- src/answers_ping.cpp
+++ src/answers_ping.cpp
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 16:02:52 by jrollon-          #+#    #+#             */
-/*   Updated: 2026/02/03 17:45:40 by jrollon-         ###   ########.fr       */
+/*   Updated: 2026/02/04 11:34:35 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,18 @@
 void IRCServ::answer_ping(IRCMessage & msg, int fd) {
 
 	std::string ans;
-
+			
 	vectorIteratorPairType params = msg.getParameters();
+	if (params.first == params.second){ //no hay parametros
+		if (clients.count(fd)){
+			std::string nick = clients[fd].getNick();
+			if (nick.empty())
+				nick = "*"; //se usa como placeholder cuando no hay nick asociado
+			ans = ":irc.42server.local 461 " + nick + " PING :Not enough parameters\r\n";
+			queue_and_send(fd, ans);
+		}
+		return ;	
+	}
 	ans = std::string("PONG ") + "irc.42server.local" + " :" + *(params.first) + "\r\n"; 
-	
 	queue_and_send(fd, ans);
 }
