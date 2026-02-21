b7482b28cc7fdf6103e66e1f3e25844d4871399f
Michelangelo Vassallo <mvassall@student.42madrid.com>
2026-02-21 15:23:09 +0100

    bot recognizes invalid pass and nick already in use

--- src/konfu.cpp
+++ src/konfu.cpp
@@ -1,4 +1,6 @@
 #include <arpa/inet.h>
+#include <cerrno>
+#include <csignal>
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
@@ -14,6 +16,7 @@
 using std::cerr;
 using std::endl;
 using std::string;
+using std::vector;
 
 /** Parse a string to int value using re /' '*[-]d+/
  * @param {const std::string} input
@@ -63,6 +66,36 @@ std::vector<string> readPhrases(const char* filename) {
   return lines;
 }
 
+volatile sig_atomic_t receivedSignal = 0;
+void signalHandler(int signum) {
+   receivedSignal = signum;
+}
+
+void installSigHandler() {
+  struct sigaction sigIntAction;
+  sigIntAction.sa_handler = signalHandler;
+  sigemptyset(&sigIntAction.sa_mask);
+  sigIntAction.sa_flags = 0;
+  sigaction(SIGINT, &sigIntAction, NULL);
+  sigaction(SIGQUIT, &sigIntAction, NULL);
+  sigaction(SIGPIPE, &sigIntAction, NULL);
+}
+
+ssize_t replyPRIVMSG(string& line, const vector<string>phrases, int sockFD) {
+  size_t startPos = line.find_first_of(':');
+  size_t endPos = line.find_first_of('!');
+  if (startPos == string::npos || endPos == string::npos 
+    || startPos >= endPos || startPos == endPos - 1) {
+    cerr << "ERROR: empty nick received in PRIVMSG" << endl;
+    return 0;
+  }
+  string nick = line.substr(startPos+1,endPos-startPos-1);
+  string reply = "privmsg ";
+  reply.append(nick).append(" :").append(phrases[rand() % phrases.size()])
+    .append("\r\n");
+  return send(sockFD,reply.c_str(),reply.length(),MSG_NOSIGNAL);
+}
+
 int main(int ac, char* av[]) {
   if (ac != 6) {
     cerr << "Usage: " << av[0] <<" server port pass nick lines-file" << endl;
@@ -88,8 +121,8 @@ int main(int ac, char* av[]) {
   struct sockaddr_in serv_addr;
 
   // Create socket
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0) {
+  int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockFD < 0) {
     cerr << "Socket creation error" << endl;
     return 4;
   }
@@ -99,75 +132,78 @@ int main(int ac, char* av[]) {
 
   // Convert IPv4 and IPv6 addresses from text to binary
   if (inet_pton(AF_INET, serverName, &serv_addr.sin_addr) <= 0) {
-    cerr << "Invalid address/ Address not supported" << endl;
+    cerr << "Invalid address/Address not supported" << endl;
     return 5;
   }
 
   // Connect to server
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+  if (connect(sockFD, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
     cerr << "Connection Failed" << endl;
     return 6;
   }
   // Send data
-  string msg = "pass "; msg.append(pass); msg.append("\r\n");
-  msg.append("nick "); msg.append(nick); msg.append("\r\n");
-  msg.append("user "); msg.append(nick); msg.append(" 0 * :Konfucius\r\n");
-  if (send(sock, msg.c_str(), msg.length(), MSG_NOSIGNAL) < 0) {
-    close(sock);
+  FILE* sockFILE = fdopen(sockFD, "r");
+  if (!sockFILE) {
+      cerr << "fdopen" << endl;
+      close(sockFD);
+      return 8;
+  }
+  string msg = "pass "; 
+  msg.append(pass).append("\r\n")
+    .append("nick ").append(nick).append("\r\n")
+    .append("user ").append(nick).append(" 0 * :Konfucius\r\n");
+  if (send(sockFD, msg.c_str(), msg.length(), MSG_NOSIGNAL) < 0) {
+    close(sockFD);
     cerr << "send error login" << endl;
     return 7;
   }
-  FILE* file = fdopen(sock, "r+");
-  if (!file) {
-      cerr << "fdopen" << endl;
-      close(sock);
-      return 8;
-  }
   char* buf = NULL;
   size_t lineLen = 0;
-  bool keepGoing = true;
-  while (keepGoing && getline(&buf, &lineLen, file) >= 0) {
-    string line = buf;
-    free(buf);
-    buf = NULL;
-    // :bbb!BBB@127.0.0.1 PRIVMSG aaa :hello
-    if (line.find(" PRIVMSG ") == string::npos) {
-      line.clear(); continue;
-    }
-    if (line.find("SHUTDOWN") != string::npos) {
-      string quitMsg = "quit :bye bye bye bye bye bye bye bye\r\n";
-      send(sock,quitMsg.c_str(),quitMsg.length(),MSG_NOSIGNAL);
-      shutdown(sock,SHUT_WR);
-      keepGoing = false;
+  installSigHandler();
+  while (receivedSignal == 0) {
+    if (getline(&buf, &lineLen, sockFILE) <= 0) {
+      cerr << endl << "getline errno: " << errno << endl;
       continue;
     }
-    size_t startPos = line.find_first_of(':');
-    size_t endPos = line.find_first_of('!');
-    if (startPos == string::npos || endPos == string::npos || startPos >= endPos) {
-      line.clear(); continue;
+    string line = buf;
+    free(buf); buf = NULL;
+    // :bbb!BBB@127.0.0.1 PRIVMSG aaa :hello
+    if (line.find(" PRIVMSG ") != string::npos) {
+      if (replyPRIVMSG(line, phrases, sockFD) < 0)
+        receivedSignal = -1; 
+      continue;
     }
-    string nick = line.substr(startPos+1,endPos-startPos-1);
-    string reply = "privmsg ";
-    reply.append(nick);
-    reply.append(" :");
-    reply.append(phrases[rand() % phrases.size()]);
-    reply.append("\r\n");
-    if (send(sock,reply.c_str(),reply.length(),MSG_NOSIGNAL) < 0) {
-      keepGoing = false;
+    string errorMsg;
+    if (line.find("464 ") != string::npos) {
+      errorMsg = "FATAL: Invalid password";
+    } else if (line.find("433 ") != string::npos) {
+      errorMsg = "FATAL: Nickname already in use";
+    }
+    if (!errorMsg.empty()) {
+      fclose(sockFILE);
+      close(sockFD);
+      cerr << errorMsg << endl;
+      return 9;
     }
   }
   free(buf);
+  if (receivedSignal != 0) cerr << "Received signal " << receivedSignal << endl;
+  string quitMsg = "quit :leaving, ciao\r\n";
+  if (send(sockFD,quitMsg.c_str(),quitMsg.length(),MSG_NOSIGNAL) < 0) {
+    cerr << "send quit returned < 0" << endl;
+  }
+  shutdown(sockFD,SHUT_WR);
   buf = NULL;
-  while (getline(&buf, &lineLen, file) > 0) {
+  while (getline(&buf, &lineLen, sockFILE) > 0) {
     cerr << buf;
     free(buf);
     buf = NULL;
   }
   free(buf);
   // Close socket
-  shutdown(sock, SHUT_RD);
-  fclose(file);
-  close(sock);
+  shutdown(sockFD, SHUT_RD);
+  fclose(sockFILE);
+  close(sockFD);
   return 0;
 }
 
